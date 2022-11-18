7e948db19435b58c053bf6f906d52a60a6a5b263
Otkisaev <polarevia@bk.ru>
2022-11-18 17:45:51 +0300

    база

---
+++ zahotel/zahotel/src/test/java/com/hotel/v/zahotel/ZahotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.v.zahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZahotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
